80d7fb4a09115fb9a8efb817fefea54b1285e7fd
Ivliro <miro.krotky@gmail.com>
2024-06-04 08:10:59 +0200

    first commit i believe?

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # maze-solver
+this project will generate a maze and also solve it i presume using few different algorithms
---
+++ src/main.py
@@ -0,0 +1,8 @@
+from window import Window
+
+def main():
+    #print('hello world')
+    win = Window(800,600)
+    win.wait_for_close()
+
+main()
